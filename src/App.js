fbc7250e032982aba9621a5afef6ac168b2737bb
kiurd-gentelman <k.r.imtiaz@gmail.com>
2022-05-31 22:48:33 +0600

    new code import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import 'antd/dist/antd.min.css';
+import {Button, DatePicker, Result, version} from "antd";
+import { Skeleton } from 'antd';
 
-import MergeImage from './MergeImage';
 
 function App() {
+
+    const ExtraHtml = () => {
+      return(
+          <div>
+              <h1>Hello</h1>
+              <DatePicker />
+              <Button type="primary" style={{ marginLeft: 8 }}>
+                  Primary Button
+              </Button>
+          </div>
+      )
+    }
   return (
     <div className="App">
-        <MergeImage />
+
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+
+        <Result
+            status="403"
+            title="403"
+            subTitle="Sorry, you are not authorized to access this page."
+            extra={<ExtraHtml/>}
+        />,
+        <Skeleton />
+
     </div>
   );
 }
